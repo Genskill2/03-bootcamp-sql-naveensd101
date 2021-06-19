9ce86ef4d7ec9102d1dccf8cebe1009dcf40e066
Naveen <naveensd101@gmail.com>
2021-06-19 21:03:20 +0530

    bs.books to bs.book

--- query3.sql
+++ query3.sql
@@ -1,6 +1,6 @@
 select b.title
 from books b, subjects s, books_subjects bs
-where b.id = bs.books and 
+where b.id = bs.book and 
 	  bs.subject = s.id and 
 	  s.name in ("Technology", "Politics");
 
--- query4.sql
+++ query4.sql
@@ -1,6 +1,6 @@
 select s.name
 from subjects s, books_subjects bs, books b
-where bs.books = b.id and
+where bs.book = b.id and
 	  bs.subject = s.id and
 	  b.title="Atomic Habits";
 
