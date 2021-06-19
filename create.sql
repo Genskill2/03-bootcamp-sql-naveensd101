0121f6bbb5f031e86358a292b41e10f925247d9a
Naveen <naveensd101@gmail.com>
2021-06-19 20:33:11 +0530

    created tables

---
+++ create.sql
@@ -0,0 +1,22 @@
+CREATE TABLE publisher (
+	id integer primary key,
+	name text,
+	country text
+);
+
+CREATE TABLE books (
+	id integer primary key,
+	title text,
+	publisher integer references publisher(id)
+);
+
+CREATE TABLE subjects (
+	id integer primary key,
+	name text
+);
+
+CREATE TABLE books_subjects (
+	books integer references books(id),
+	subject integer references subjects(id)
+);
+
